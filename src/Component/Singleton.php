8821840fb29833e02b9a248d4c2147e46269d5d6
darifo <feaglenet@gmail.com>
2020-07-04 15:13:22 +0800

    a

---
+++ src/Component/Singleton.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Darifo\Gateway\Component;
+
+
+trait Singleton
+{
+    private static $instance;
+
+    static function getInstance(...$args)
+    {
+        if(!isset(self::$instance)){
+            self::$instance = new static(...$args);
+        }
+        return self::$instance;
+    }
+}
